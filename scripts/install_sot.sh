5e3846087021c6bf07db7be9d7861ee425e7b9d0
Francois Keith <keith@lirmm.fr>
2013-11-12 23:56:54 +0100

    The config file has now its own step.

--- scripts/install_sot.sh
+++ scripts/install_sot.sh
@@ -331,6 +331,9 @@ create_local_db()
   inst_array[index]="install_ros_ws"
   let "index= $index +1"
 
+  inst_array[index]="install_config"
+  let "index= $index +1"
+
   inst_array[index]="install_pkg $SRC_DIR/robots romeo-sot.git ${INRIA_URI}"
   let "index= $index + 1"
 
@@ -858,9 +861,6 @@ install_ros_ws()
            "   local-name: stacks/hrp4\n    version: "${ROS_VERSION} > /tmp/idh-private.rosinstall
       rosinstall $SOT_ROOT_DIR  /tmp/idh-private.rosinstall
     fi
-
-    # create the config file.
-    install_config
 }
 
 install_ros_ws_package()
